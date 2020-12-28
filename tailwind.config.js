87195b1951aacc4f6df8b0a892e72d37a75d1c83
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2020-12-28 00:18:21 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: {
+    mode: "all",
+    content: ["./src/**/*.{js,mdx}", "./next.config.js"],
+    options: {
+      extractors: [
+        {
+          extensions: ["mdx"],
+          extractor: (content) => {
+            content = mdx.sync(content);
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
+
+            return broadMatches.concat(innerMatches);
+          },
+        },
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     // screens: {
